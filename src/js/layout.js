0e0ca3983d217b1e069603aea3ad6003c7679550
arixvi <ari.villarreal96@icloud.com>
2023-04-14 00:04:37 +0000

    comment

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
+git 
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
